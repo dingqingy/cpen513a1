fc15e4a2454d690191c1b84a0c085a83f4bfc96f
Dingqing Yang <williamyang1203@gmail.com>
2020-01-24 14:55:19 -0800

    update backtracking

--- routing.py
+++ routing.py
@@ -31,7 +31,7 @@ class Router:
         if(is_target_found):
             print('label map')
             print(self.label.T)
-            solution = self.traceBack(source, target)
+            solution = self.backTrack(target, source)
             print('routing solution: {}'.format(solution))
         else:
             print('not routable')
@@ -48,11 +48,11 @@ class Router:
         # make sure we don't go out of bounds
         if x > 0:
             self.expand_((x - 1, y), new_score)
-        if x < x_bound-1:
+        if x < x_bound - 1:
             self.expand_((x + 1, y), new_score)
         if y > 0:
             self.expand_((x, y - 1), new_score)
-        if y < y_bound-1:
+        if y < y_bound - 1:
             self.expand_((x, y + 1), new_score)
 
     # low level expand
@@ -62,45 +62,60 @@ class Router:
             self.expansion_list.put((score, coordinate))
 
     # This only goes forward, need to have a way to go back
-    def traceBack(self, source, target):
+    def backTrack(self, start, end):
+        visited = np.zeros(self.grid_size, dtype=bool)
         result = []
-        x, y = current_loc = target
+        # print(visited.T)
+        # print(start)
+        # print(visited[start])
+        if self.backTrack_(start, end, visited, result):
+            return result
+        else:
+            print('backtrack failed')
+            assert False
+
+    def backTrack_(self, start, end, visited, result):
+        if(start == end):
+            # print('start hit end')
+            result.append(start)
+            # print(result)
+            return True
+        # explore neighbours
+        visited[start] = True
+        x, y = start
+        current_score = self.label[start]
+        # print('current position', start)
+        # print('current score', current_score)
         x_bound, y_bound = self.grid_size
-        current_score = self.label[target]
-        print('starting location', current_loc)
-        print('starting score', current_score)
-
-        while True:
-            # how to enforce order?
-            # based on different algorithm, new score can be calculated differently
-            x, y = current_loc
-            if current_loc == source:
-                break
-            elif x > 0 and self.label[x - 1, y] == current_score - 1:
-                result.append((x - 1, y))
-                current_loc = (x - 1, y)
-                current_score -= 1
-            elif x<x_bound-1 and self.label[x + 1, y] == current_score - 1:
-                result.append((x + 1, y))
-                current_loc = (x + 1, y)
-                current_score -= 1
-            elif y>0 and self.label[x, y - 1] == current_score - 1:
-                result.append((x, y - 1))
-                current_loc = (x, y - 1)
-                current_score -= 1
-            elif y<y_bound-1 and self.label[x, y + 1] == current_score - 1:
-                result.append((x, y + 1))
-                current_loc = (x, y + 1)
-                current_score -= 1
-            else:
-                print('no way to trace back, something is wrong!')
-                print('current result', result)
-                assert False
-
-            print('location updated to', current_loc)
-            print('score updated to', current_score)
-            # print(self.label[x, y - 1])
-        return result
+        if x > 0:
+            new_start = (x - 1, y)
+            if self.label[new_start] < current_score and not visited[new_start]:
+                if self.backTrack_(new_start, end, visited, result):
+                    result.append(start)
+                    # print(result)
+                    return True
+        if y > 0:
+            new_start = (x, y - 1)
+            if self.label[new_start] < current_score and not visited[new_start]:
+                if self.backTrack_(new_start, end, visited, result):
+                    result.append(start)
+                    # print(result)
+                    return True
+        if x + 1 < x_bound:
+            new_start = (x + 1, y)
+            if self.label[new_start] < current_score and not visited[new_start]:
+                if self.backTrack_(new_start, end, visited, result):
+                    result.append(start)
+                    # print(result)
+                    return True
+        if y + 1 < y_bound:
+            new_start = (x, y + 1)
+            if self.label[new_start] < current_score and not visited[new_start]:
+                if self.backTrack_(new_start, end, visited, result):
+                    result.append(start)
+                    # print(result)
+                    return True
+        return False
 
 
 if __name__ == '__main__':
